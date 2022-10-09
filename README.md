ff9bb27b9abf2f8393f0131adb7a67377076d6f3
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2022-10-09 13:27:51 +0800

    create

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
 # np-start
-Naiveproxy server automatically compiles scripts
+
+Ubuntu20.04+ 自动编译 Naiveproxy
+
+## Quick Start
+
+
+
+## 脚本执行流
+
+### 预准备
+
+1. 检查 apt packages: snap, wget
+
+   ```shell
+   apt install -y snapd wget
+   ```
+
+2. 确保 80,443 端口无占用
+
+   通常认为是 nginx 占用
+
+   ```shell
+   nginx -s stop
+   ```
+
+   或通过以下指令检查端口占用
+
+   ```shell
+   lsof -i:80
+   lsof -i:443
+   ```
+
+### 自动编译
+
+1. 安装 golang 1.18.5
+
+   ```shell
+   snap install go --classic
+   ```
+
+2. 安装 xcaddy
+
+   ```shell
+   wget https://github.com/caddyserver/xcaddy/releases/download/v0.3.1/xcaddy_0.3.1_linux_amd64.deb && apt install -y ./xcaddy_0.3.1_linux_amd64.deb && rm xcaddy_0.3.1_linux_amd64.deb
+   ```
+
+
+3. 编译 Naiveproxy
+
+   根据硬件配置需要 1 ~ 5分钟。
+
+   编译结束后，在 output 目录下生成一个名为 `caddy` 的二进制文件，通过它运行携带 Naiveproxy 的 Caddy2 服务器。
+
+   ```shell
+   xcaddy build --output /home/caddy --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
+   ```
+
+### 编写配置文件
+
+编写 Caddyfile，在 output 目录下创建一个名为 Caddyfile 的文本文件，这里写入 Naiveproxy 的基础配置。
+
+配置文件中的 `[占位符]` 需要手动替换，当然默认的运行端口（443）也可以更改，但不推荐，除非情况特殊。此时你需要使用 `config` 模版文件，请访问 Caddy 官方文档获取更多信息。
+
+```wiki
+:443, [domain]
+tls [email]
+route {
+  forward_proxy {
+   basic_auth [username] [password]
+   hide_ip
+   hide_via
+   probe_resistance
+  }
+  reverse_proxy https://demo.cloudreve.org   {
+   header_up  Host  {upstream_hostport}
+   header_up  X-Forwarded-Host  {host}
+  }
+}
+```
+
+### 启动 Naiveproxy
+
+`/home/caddy` 为 output 输出路径，根据你自己的参数而定。通过 `caddy --help` 查看所有指令
+
+1. 前台运行
+
+   ```shell
+   /home/caddy run
+   ```
+
+2. 后台运行
+
+   ```shell
+   /home/caddy start
+   ```
+
+3. 开机自启
+
+##  客户端配置
+
+使用图形化界面运行 Naiveproxy Core：
+
+- Windows: NekoRay，V2RayN
+
+- Android: Matsuri
+
+- iOS: shadowrocket
+
---
+++ main.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+# Time       : 2022/10/9 13:06
+# Author     : QIN2DIM
+# Github     : https://github.com/QIN2DIM
+# Description:
+import logging
+import os
+import random
+import sys
+import uuid
+from dataclasses import dataclass
+
+# 阻止 python2 及非 linux 系统运行
+if sys.version_info[0] < 3 or sys.platform != "linux":
+    sys.exit()
+
+CADDYFILE_TEMPLATE = """
+:443, [domain]
+tls [email]
+route {
+  forward_proxy {
+   basic_auth [username] [password]
+   hide_ip
+   hide_via
+   probe_resistance
+  }
+  reverse_proxy https://demo.cloudreve.org   {
+   header_up  Host  {upstream_hostport}
+   header_up  X-Forwarded-Host  {host}
+  }
+}
+"""
+
+V2RAYN_TEMPLATE = """
+{
+    "listen": "socks://127.0.0.1:[listen_port]",
+    "proxy": "https://[username]:[password]@[domain]"
+}
+"""
+
+NEKORAY_TEMPLATE = """
+-- 通用 -- 
+名称: NaiveTurtle
+地址: [domain]
+端口: [port]
+-- Naive --
+用户名: [username]
+密码: [password]
+协议: https
+SNI: [domain]
+不安全并发: 0
+"""
+
+
+@dataclass
+class Config:
+    dir_workspace = "/home/naiveproxy"
+    dir_conf = os.path.join(dir_workspace, "conf")
+    path_caddyfile = os.path.join(dir_workspace, "Caddyfile")
+    path_caddy = os.path.join(dir_workspace, "caddy")
+    username = ""
+    password = ""
+    email = ""
+    domain = ""
+    sni = ""
+    port = "443"
+
+    def __post_init__(self):
+        os.makedirs(self.dir_workspace, exist_ok=True)
+        os.makedirs(self.dir_conf, exist_ok=True)
+
+
+config = Config()
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] %(message)s',
+                    datefmt='%Y/%m/%d %H:%M:%S')
+
+
+def preprocess():
+    logging.info("Check snap, wget, port80 and port443")
+
+    cmd_queue = (
+        "apt install -y snapd wget >/dev/null 2>&1",
+        "nginx -s stop >/dev/null 2>&1"
+    )
+    for cmd in cmd_queue:
+        os.system(cmd)
+
+
+def handle_server():
+    logging.info("Check go1.18+")
+    os.system("snap install go --classic >/dev/null 2>&1")
+
+    logging.info("Check xcaddy ")
+    cmd_queue = (
+        "wget https://github.com/caddyserver/xcaddy/releases/download/v0.3.1/xcaddy_0.3.1_linux_amd64.deb >/dev/null 2>&1",
+        "apt install -y ./xcaddy_0.3.1_linux_amd64.deb >/dev/null 2>&1",
+        "rm xcaddy_0.3.1_linux_amd64.deb",
+    )
+    for cmd in cmd_queue:
+        os.system(cmd)
+
+    if not os.path.isfile(config.path_caddy):
+        logging.info("Build caddy with naiveproxy")
+        os.system(
+            f"xcaddy build "
+            f"--output {config.path_caddy} "
+            f"--with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive"
+        )
+    else:
+        logging.info("Caddy already exists, skip compilation")
+
+
+def guider_input():
+    """引导输入，生成配置模版"""
+
+    def output(template):
+        with open(config.path_caddyfile, "w", encoding="utf8") as file:
+            file.write(template)
+
+    os.system("clear")
+    while not (domain := input("请输入域名 ---> ").strip()):
+        pass
+
+    tmp = CADDYFILE_TEMPLATE
+    config.domain = domain
+    config.username = f"{uuid.uuid4().hex}"[:8]
+    config.password = uuid.uuid4().hex
+    config.email = f"{str(uuid.uuid4().hex)[:8]}@tesla.com"
+    placeholder2val = {
+        "[domain]": domain,
+        "[email]": config.email,
+        "[username]": config.username,
+        "[password]": config.password,
+    }
+    for placeholder in placeholder2val:
+        tmp = tmp.replace(placeholder, placeholder2val[placeholder])
+
+    print(f" Caddyfile ".center(50, "="), tmp)
+
+    output(tmp)
+
+
+def dropout_client_config_v2rayn():
+    tmp = V2RAYN_TEMPLATE
+    p2v = {
+        "[domain]": config.domain,
+        "[username]": config.username,
+        "[password]": config.password,
+        "[listen_port]": f"{random.randint(50000, 60000)}"
+    }
+    for p in p2v:
+        tmp = tmp.replace(p, p2v[p])
+
+    print(" V2RayN ".center(50, "="), tmp)
+    path_output = os.path.join(config.dir_conf, "v2rayn_naive.json")
+    with open(path_output, "w", encoding="utf8") as file:
+        file.write(tmp)
+
+
+def dropout_client_config_nekoray():
+    tmp = NEKORAY_TEMPLATE
+    p2v = {
+        "[domain]": config.domain,
+        "[username]": config.username,
+        "[password]": config.password,
+        "[port]": config.port,
+    }
+    for p in p2v:
+        tmp = tmp.replace(p, p2v[p])
+
+    print(" NekoRay ".center(50, "="), tmp)
+    path_output = os.path.join(config.dir_conf, "nekoray_naive.txt")
+    with open(path_output, "w", encoding="utf8") as file:
+        file.write(tmp)
+
+
+def autorun():
+    logging.info(f"测试稳定后运行 {config.path_caddy} start 部署后台任务")
+    input("按任意键试运行 naiveproxy，之后可以 CTRL+C 退出进程")
+    os.system(f"cd {config.dir_workspace} && ./caddy run")
+
+
+if __name__ == '__main__':
+    preprocess()
+    handle_server()
+    guider_input()
+    dropout_client_config_v2rayn()
+    dropout_client_config_nekoray()
+    autorun()
