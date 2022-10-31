afed5969b3b2d98d49d828c0be98e3e269c5563b
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2022-11-01 07:08:35 +0800

    v0.1.0 (#1)
    v0.1.0
    
    * feat(add): naiveproxy system service
    添加系统服务
    
    * feat(add): network finetune
    添加可控的 HTTP/2 网络优化策略
    
    * feat(add): CMDPanel
    添加命令行管理面板，除自动编译外，加入 Naiveproxy 系统服务控制及脚本更新功能

--- README.md
+++ README.md
@@ -1,113 +1,29 @@
-# np-start
+# npstart
 
-Ubuntu20.04+ 自动编译 Naiveproxy
+Ubuntu20.04+ 一键编译 Naiveproxy
 
 ## Quick Start
 
 ```shell
-wget -O npstart.py https://raw.githubusercontent.com/QIN2DIM/np-start/main/main.py && python3 npstart.py
+wget -qO /home/npstart.py https://raw.githubusercontent.com/QIN2DIM/np-start/main/main.py && python3 /home/npstart.py
 ```
 
-## 脚本执行流
+之后可通过快捷指令 `npstart` 运行脚本。
 
-### 预准备
+## Features
 
-1. 检查 apt packages: snap
+- [x] 自动编译最新版 Naiveproxy，注册系统服务，脚手架模式管理系统服务
+- [x] 自动配置 BBR+FQ 拥塞控制策略
+- [x] 优化 HTTP/2 网络环境，大幅度提升吞吐量，降低延时
+- [ ] 监听、拉取、更新代理核心
 
-   ```shell
-   apt install -y snapd
-   ```
+## Requirement
 
-2. 确保 80,443 端口无占用
+[Run Caddy as a daemon · klzgrad/naiveproxy Wiki](https://github.com/klzgrad/naiveproxy/wiki/Run-Caddy-as-a-daemon)
 
-   通常认为是 nginx 占用
+[Optimizing HTTP/2 prioritization with BBR and tcp_notsent_lowat](https://blog.cloudflare.com/http-2-prioritization-with-nginx/)
 
-   ```shell
-   nginx -s stop
-   ```
+[Optimizing TCP for high WAN throughput while preserving low latency | Noise](https://noise.getoto.net/2022/07/01/optimizing-tcp-for-high-wan-throughput-while-preserving-low-latency/)
 
-   或通过以下指令检查端口占用
-
-   ```shell
-   lsof -i:80
-   lsof -i:443
-   ```
-
-### 自动编译
-
-1. 安装 golang 1.18.5
-
-   这一步执行前会先通过 `apt remove -y golang-go` 卸载低版本语言包。
-
-   ```shell
-   snap install go --classic
-   ```
-
-2. 安装 xcaddy
-
-   ```shell
-   wget https://github.com/caddyserver/xcaddy/releases/download/v0.3.1/xcaddy_0.3.1_linux_amd64.deb && apt install -y ./xcaddy_0.3.1_linux_amd64.deb && rm xcaddy_0.3.1_linux_amd64.deb
-   ```
-
-
-3. 编译 Naiveproxy
-
-   根据硬件配置需要 1 ~ 5分钟。
-
-   编译结束后，在 output 目录下生成一个名为 `caddy` 的二进制文件，通过它运行携带 Naiveproxy 的 Caddy2 服务器。
-
-   ```shell
-   xcaddy build --output /home/caddy --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
-   ```
-
-### 编写配置文件
-
-编写 Caddyfile，在 output 目录下创建一个名为 Caddyfile 的文本文件，这里写入 Naiveproxy 的基础配置。
-
-配置文件中的 `[占位符]` 需要手动替换，当然默认的运行端口（443）也可以更改，但不推荐，除非情况特殊。此时你需要使用 `config` 模版文件，请访问 Caddy 官方文档获取更多信息。
-
-```wiki
-:443, [domain]
-tls [email]
-route {
-  forward_proxy {
-   basic_auth [username] [password]
-   hide_ip
-   hide_via
-   probe_resistance
-  }
-  reverse_proxy https://demo.cloudreve.org   {
-   header_up  Host  {upstream_hostport}
-   header_up  X-Forwarded-Host  {host}
-  }
-}
-```
-
-### 启动 Naiveproxy
-
-`/home/caddy` 为 output 输出路径，根据你自己的参数而定。通过 `caddy --help` 查看所有指令。
-
-1. 前台运行
-
-   ```shell
-   cd /home && ./caddy run
-   ```
-
-2. 后台运行
-
-   ```shell
-   cd /home && ./caddy start
-   ```
-
-3. 开机自启
-
-##  客户端配置
-
-使用图形化界面运行 Naiveproxy Core：
-
-- Windows: NekoRay，V2RayN
-
-- Android: Matsuri
-
-- iOS: shadowrocket
+[Caddyfile Concepts — Caddy Documentation](https://caddyserver.com/docs/caddyfile/concepts#structure)
 
--- main.py
+++ main.py
@@ -3,16 +3,34 @@
 # Author     : QIN2DIM
 # Github     : https://github.com/QIN2DIM
 # Description:
+import base64
+import functools
+import json
 import logging
 import os
-import random
+import re
 import sys
+import time
+import typing
 import uuid
 from dataclasses import dataclass
 
 # 阻止 python2 及非 linux 系统运行
 if sys.version_info[0] < 3 or sys.platform != "linux":
     sys.exit()
+os.system("clear")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y/%m/%d %H:%M:%S",
+)
+
+WORKSPACE = "/home/naiveproxy/"
+PATH_CADDY = os.path.join(WORKSPACE, "caddy")
+PATH_CADDYFILE = os.path.join(WORKSPACE, "Caddyfile")
+LOCAL_SCRIPT = "/home/npstart.py"
+REMOTE_GITHUB = "https://raw.githubusercontent.com/QIN2DIM/np-start/main/main.py"
 
 CADDYFILE_TEMPLATE = """
 :443, [domain]
@@ -33,167 +51,396 @@ route {
 
 V2RAYN_TEMPLATE = """
 {
-    "listen": "socks://127.0.0.1:[listen_port]",
-    "proxy": "https://[username]:[password]@[domain]"
+    "listen": "socks://127.0.0.1:58185",
+    "proxy": "[proxy]"
 }
 """
 
-NEKORAY_TEMPLATE = """
--- 通用 -- 
-名称: NaiveTurtle
-地址: [domain]
-端口: [port]
--- Naive --
-用户名: [username]
-密码: [password]
-协议: https
-SNI: [domain]
-不安全并发: 0
+GUIDER_PANEL = """ -------------------------------------------
+|**********        npstart         **********|
+|**********    Author: QIN2DIM     **********|
+|**********     Version: 0.1.0     **********|
+ -------------------------------------------
+Tips: npstart 命令再次运行本脚本.
+.............................................
+
+############################### 
+
+..................... 
+1)  敏捷部署 Naiveproxy 
+2)  卸载 
+..................... 
+3)  启动 
+4)  暂停 
+5)  重载 
+6)  运行状态 
+..................... 
+7)  查看当前配置 
+8)  重新配置
+..................... 
+9)  更新 npstart
+10  [dev] sync upstream
+
+############################### 
+
+
+
+0)退出 
+............................................. 
+请选择: """
+
+NAIVEPROXY_SERVICE = f"""
+[Unit]
+Description=npstart:Caddy2 web server with naiveproxy plugin
+Documentation=https://github.com/QIN2DIM/np-start
+After=network.target network-online.target
+Requires=network-online.target
+
+[Service]
+# User=naiveproxy
+# Group=naiveproxy
+ExecStart={PATH_CADDY} run --environ --config {PATH_CADDYFILE}
+ExecReload={PATH_CADDY} reload --config {PATH_CADDYFILE}
+TimeoutStopSec=5s
+LimitNOFILE=1048576
+LimitNPROC=512
+PrivateTmp=true
+ProtectSystem=full
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+# Restart=always
+# RestartSec=45s
+
+[Install]
+WantedBy=multi-user.target
+"""
+
+SHELL_NPSTART = f"""
+if [ ! -f "{LOCAL_SCRIPT}" ]; then
+    echo "Local script is missing, trying to sync upstream content"
+    wget -qO {LOCAL_SCRIPT} {REMOTE_GITHUB}
+fi
+python3 {LOCAL_SCRIPT}
 """
 
 
 @dataclass
-class Config:
-    dir_workspace = "/home/naiveproxy"
-    dir_conf = os.path.join(dir_workspace, "conf")
-    path_caddyfile = os.path.join(dir_workspace, "Caddyfile")
-    path_caddy = os.path.join(dir_workspace, "caddy")
-    username = ""
-    password = ""
-    email = ""
-    domain = ""
-    sni = ""
-    port = "443"
-    scheme = "https"  # "https" or "quic"
+class CaddyServer:
+    username: str = f"{uuid.uuid4().hex}"[:8]
+    password: str = uuid.uuid4().hex
+    email: str = f"{str(uuid.uuid4().hex)[:8]}@tesla.com"
+    domain: str = ""
+    port: str = "443"
+    scheme: str = "https"  # "https" or "quic"
+
+    def get_caddyfile(self):
+        caddyfile = CADDYFILE_TEMPLATE.strip()
+        p2v = {
+            "[domain]": self.domain,
+            "[email]": self.email,
+            "[username]": self.username,
+            "[password]": self.password,
+        }
+        for placeholder in p2v:
+            caddyfile = caddyfile.replace(placeholder, p2v[placeholder])
+        return caddyfile
+
+    def get_v2rayn_custom_server(self):
+        v2rayn_custom_server = V2RAYN_TEMPLATE.strip()
+        proxy = f"{self.scheme}://{self.username}:{self.password}@{self.domain}"
+        return v2rayn_custom_server.replace("[proxy]", proxy)
+
+    def get_nekoray_sharelink(self):
+        typecode = f"{self.username}:{self.password}@{self.domain}:{self.port}#{self.domain}"
+        sharelink = f"naive+{self.scheme}://{typecode}"
+        return sharelink
+
+    def get_shadowrocket_sharelink(self):
+        typecode = f"{self.username}:{self.password}@{self.domain}:{self.port}"
+        sharelink = f'http2://{base64.b64encode(typecode.encode("utf8")).decode("utf8")}'
+        return sharelink.replace("=", "")
+
+
+@dataclass
+class ClientSettings:
+    dir_workspace: str = WORKSPACE
+    path_caddyfile: str = PATH_CADDYFILE
+
+    path_config_server: typing.Optional[str] = ""
+    path_client_config: typing.Optional[str] = ""
+    caddy: typing.Optional[CaddyServer] = None
 
     def __post_init__(self):
+        self.path_config_server = os.path.join(self.dir_workspace, "caddy_server.json")
+        self.path_client_config = os.path.join(self.dir_workspace, "clients.json")
+        self.caddy = self._preload_config()
+
+    def _preload_config(self) -> CaddyServer:
         os.makedirs(self.dir_workspace, exist_ok=True)
-        os.makedirs(self.dir_conf, exist_ok=True)
+        try:
+            with open(self.path_config_server, "r", encoding="utf8") as file:
+                return CaddyServer(**json.load(file))
+        except (FileNotFoundError, KeyError, TypeError):
+            return CaddyServer()
+
+    def refresh_localcache(self, drop: bool = False):
+        """修改 Caddyfile 以及客户端配置"""
+        localcache = {
+            "v2rayn_custom_server": self.caddy.get_v2rayn_custom_server(),
+            "nekoray_sharelink": self.caddy.get_nekoray_sharelink(),
+            "shadowrocket_sharelink": self.caddy.get_shadowrocket_sharelink(),
+        }
+        with open(self.path_client_config, "w", encoding="utf8") as file:
+            json.dump(localcache, file)
+        with open(self.path_config_server, "w", encoding="utf8") as file:
+            json.dump(self.caddy.__dict__, file, indent=4)
+        with open(self.path_caddyfile, "w", encoding="utf8") as file:
+            file.write(self.caddy.get_caddyfile())
+
+        if drop:
+            print(" ↓ ↓ V2RayN ↓ ↓ ".center(50, "="))
+            print(localcache.get("v2rayn_custom_server"))
+            print(" ↓ ↓ NekoRay & Matsuri ↓ ↓ ".center(50, "="))
+            print(f"\n{localcache.get('nekoray_sharelink')}\n")
+            print(" ↓ ↓ Shadowrocket ↓ ↓ ".center(50, "="))
+            print(f"\n{localcache.get('shadowrocket_sharelink')}\n")
 
 
-config = Config()
+def check_caddy(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        if not os.path.isfile(PATH_CADDY) or not os.path.getsize(PATH_CADDY):
+            logging.error(f"Naiveproxy 未初始化，請先執行「敏捷部署」 - func={func.__name__}")
+        else:
+            return func(*args, **kwargs)
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    datefmt="%Y/%m/%d %H:%M:%S",
-)
-
-
-def preprocess():
-    os.system("clear")
-
-    logging.info("Check snap, wget, port80 and port443")
-    cmd_queue = ("apt install -y snapd wget >/dev/null 2>&1", "nginx -s stop >/dev/null 2>&1")
-    for cmd in cmd_queue:
-        os.system(cmd)
+    return wrapped
 
 
-def handle_server():
-    logging.info("Check go1.18+")
-    os.system("apt remove golang-go -y >/dev/null 2>&1")
-    os.system("snap install go --classic >/dev/null 2>&1")
+def skip_recompile(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        if os.path.isfile(PATH_CADDY) and os.path.getsize(PATH_CADDY):
+            logging.error(f"Naiveproxy 已编译，如需修改参数请执行「重新配置」 - func={func.__name__}")
+        else:
+            return func(*args, **kwargs)
 
-    logging.info("Check xcaddy ")
-    cmd_queue = (
-        "wget https://github.com/caddyserver/xcaddy/releases/download/v0.3.1/xcaddy_0.3.1_linux_amd64.deb >/dev/null 2>&1",
-        "apt install -y ./xcaddy_0.3.1_linux_amd64.deb >/dev/null 2>&1",
-        "rm xcaddy_0.3.1_linux_amd64.deb",
-    )
-    for cmd in cmd_queue:
-        os.system(cmd)
+    return wrapped
 
-    if not os.path.isfile(config.path_caddy):
-        logging.info("Build caddy with naiveproxy")
-        os.system(
-            f"xcaddy build "
-            f"--output {config.path_caddy} "
-            f"--with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive"
+
+class CaddyService:
+    NAME: str = "naiveproxy"
+
+    def __init__(self):
+        self.path_units = f"/etc/systemd/system/{self.NAME}.service"
+
+        self._on_service()
+
+    def _on_service(self):
+        if not os.path.isfile(self.path_units):
+            with open(self.path_units, "w", encoding="utf8") as file:
+                file.write(NAIVEPROXY_SERVICE)
+            os.system("systemctl daemon-reload")
+            os.system(f"systemctl enable {self.NAME} >/dev/null 2>&1")
+
+    @check_caddy
+    def caddy_start(self):
+        """后台运行 CaddyServer"""
+        os.system(f"systemctl start {self.NAME}")
+        logging.info("Start the naiveproxy")
+
+    @check_caddy
+    def caddy_stop(self):
+        """停止 CaddyServer"""
+        os.system(f"systemctl stop {self.NAME}")
+        logging.info("Stop the naiveproxy")
+
+    @check_caddy
+    def caddy_reload(self):
+        """重启 CaddyServer 重新读入配置"""
+        os.system(f"systemctl reload-or-restart {self.NAME}")
+        logging.info("Reload the naiveproxy")
+
+    @check_caddy
+    def caddy_status(self):
+        """查看 CaddyServer 运行状态"""
+        os.system(f"systemctl status {self.NAME}")
+
+    def remove(self):
+        os.system(f"systemctl stop {self.NAME}")
+        os.system(f"systemctl disable {self.NAME} >/dev/null 2>&1")
+        os.system(f"rm {self.path_units}")
+        os.system("systemctl daemon-reload")
+        logging.info("Remove the naiveproxy")
+
+
+class Alias:
+    BIN_NAME: str = "npstart"
+
+    def register(self):
+        for path_bin in [f"/usr/bin/{self.BIN_NAME}", f"/usr/sbin/{self.BIN_NAME}"]:  # unnecessary
+            if not os.path.isfile(path_bin):
+                with open(path_bin, "w", encoding="utf8") as file:
+                    file.write(SHELL_NPSTART)
+                os.system(f"chmod +x {path_bin}")
+
+    def remove(self):
+        os.system(f"rm /usr/bin/{self.BIN_NAME}")
+        os.system(f"rm /usr/sbin/{self.BIN_NAME}")
+
+
+class CMDPanel:
+    def __init__(self):
+        self.path_caddy = PATH_CADDY
+        self.csm = ClientSettings()
+        self.caddy = self.csm.caddy
+        self.utils = CaddyService()
+
+        self.alias = Alias()
+        self.alias.register()
+
+    def _compile(self):
+        # ==================== preprocess ====================
+        os.system("clear")
+        logging.info("Check snap, wget, port80 and port443")
+        cmd_queue = ("apt install -y snapd wget >/dev/null 2>&1", "nginx -s stop >/dev/null 2>&1")
+        for cmd in cmd_queue:
+            os.system(cmd)
+        # ==================== handle server ====================
+        logging.info("Check go1.18+")
+        os.system("apt remove golang-go -y >/dev/null 2>&1")
+        os.system("snap install go --classic >/dev/null 2>&1")
+
+        logging.info("Check xcaddy")
+        cmd_queue = (
+            "wget https://github.com/caddyserver/xcaddy/releases/download/v0.3.1/xcaddy_0.3.1_linux_amd64.deb >/dev/null 2>&1",
+            "apt install -y ./xcaddy_0.3.1_linux_amd64.deb >/dev/null 2>&1",
+            "rm xcaddy_0.3.1_linux_amd64.deb",
         )
-    else:
-        logging.info("Caddy already exists, skip compilation")
+        for cmd in cmd_queue:
+            os.system(cmd)
 
+        os.makedirs(os.path.dirname(self.path_caddy), exist_ok=True)
+        if not os.path.isfile(self.path_caddy):
+            logging.info("Build caddy with naiveproxy")
+            os.system(
+                f"xcaddy build "
+                f"--output {self.path_caddy} "
+                f"--with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive"
+            )
+        else:
+            logging.info("Caddy already exists, skip compilation")
+            logging.info(self.path_caddy)
 
-def guider_input():
-    """引导输入，生成配置模版"""
+        # ==================== Network FineTune ====================
+        logging.info("Naiveproxy Network Performance Tuning")
+        cmd_queue = (
+            # Enable BBR+FQ Congestion control algorithm
+            "sudo sysctl -w net.core.default_qdisc=fq"
+            "sudo sysctl -w net.ipv4.tcp_congestion_control=bbr",
+            # optimizing tcp for high wan throughput while preserving low latency
+            "sudo sysctl -w net.ipv4.tcp_slow_start_after_idle=0",
+            "sudo sysctl -w net.ipv4.tcp_rmem='8192 262144 536870912'",
+            "sudo sysctl -w net.ipv4.tcp_wmem='4096 16384 536870912'",
+            "sudo sysctl -w net.ipv4.tcp_adv_win_scale=-2",
+            "sudo sysctl -w net.ipv4.tcp_collapse_max_bytes=6291456",
+            "sudo sysctl -w net.ipv4.tcp_notsent_lowat=131072",
+        )
+        for cmd in cmd_queue:
+            os.system(cmd)
 
-    def output(template):
-        with open(config.path_caddyfile, "w", encoding="utf8") as file:
-            file.write(template)
+    @staticmethod
+    def _guide_domain(prompt: str):
+        pattern = re.compile(r"(?:\w(?:[\w\-]{0,61}\w)?\.)+[a-zA-Z]{2,6}")
+        while True:
+            domain = input(prompt).strip()
+            result = re.findall(pattern, domain)
+            if result and result.__len__() == 1:
+                return result[0]
 
-    while not (domain := input("请输入域名 ---> ").strip()):
-        pass
+    @skip_recompile
+    def deploy(self):
+        self.caddy.username = input(">> 输入用户名[username](回车随机配置)：").strip() or self.caddy.username
+        self.caddy.password = input(">> 输入密码[password](回车随机配置)：").strip() or self.caddy.password
+        self.caddy.domain = self._guide_domain(prompt=">> 输入解析到本机Ipv4的域名[domain]：")
+        self._compile()
+        if not os.path.isfile(self.path_caddy):
+            logging.error("編譯失敗")
+        else:
+            logging.info(
+                "Compiled successfully! Press any key to deploy the Naiveproxy system service."
+            )
+            input()
+            self.csm.refresh_localcache(drop=True)  # deploy
+            self.utils.caddy_start()
 
-    tmp = CADDYFILE_TEMPLATE
-    config.domain = domain
-    config.username = f"{uuid.uuid4().hex}"[:8]
-    config.password = uuid.uuid4().hex
-    config.email = f"{str(uuid.uuid4().hex)[:8]}@tesla.com"
-    placeholder2val = {
-        "[domain]": domain,
-        "[email]": config.email,
-        "[username]": config.username,
-        "[password]": config.password,
-    }
-    for placeholder in placeholder2val:
-        tmp = tmp.replace(placeholder, placeholder2val[placeholder])
+    @check_caddy
+    def delete(self):
+        """删除 np-start 缓存"""
+        if input(">> 卸载「已编译的Caddy服務及缓存數據」[y/n] ").strip().lower().startswith("y"):
+            self.utils.remove()
+            self.alias.remove()
+            os.system(f"rm -rf {WORKSPACE} >/dev/null 2>&1")
+            logging.info("Delete cache of the naiveproxy")
+        else:
+            logging.info(f"Withdraw operation")
 
-    print(f" Caddyfile ".center(50, "="), tmp)
+    @check_caddy
+    def checkout(self):
+        """查看客户端配置信息"""
+        self.csm.refresh_localcache(drop=True)  # check
 
-    output(tmp)
+    @check_caddy
+    def reset(self):
+        if input(">> 是否使用上次配置的用戶名？[y/n] ").strip().lower().startswith("n"):
+            self.caddy.username = input(">> 输入用户名[username](回车随机配置)：").strip()
+        if input(">> 是否使用上次配置的密碼？[y/n] ").strip().lower().startswith("n"):
+            self.caddy.password = input(">> 输入密码[password](回车随机配置)：").strip()
+        if input(f">> 是否使用上次配置的域名({self.caddy.domain})？[y/n] ").strip().lower().startswith("n"):
+            self.caddy.domain = self._guide_domain(prompt=">> 输入解析到本机Ipv4的域名[domain]：")
+        self.csm.refresh_localcache()  # reset
+        logging.info("reset user config")
+        self.utils.caddy_reload()
 
+    def upgrade(self):
+        # TODO checkout branch version
+        logging.info("Updating script ...")
+        time.sleep(1)
+        bak = f"{LOCAL_SCRIPT}.bak"
+        os.system(f"wget -qO {bak} {REMOTE_GITHUB}")
+        if os.path.isfile(bak) and os.path.getsize(bak):
+            os.system(f"mv {bak} {LOCAL_SCRIPT}")
+        os.system(self.alias.BIN_NAME)
 
-def dropout_client_config_v2rayn():
-    tmp = V2RAYN_TEMPLATE
-    p2v = {
-        "[domain]": config.domain,
-        "[username]": config.username,
-        "[password]": config.password,
-        "[listen_port]": f"{random.randint(50000, 60000)}",
-    }
-    for p in p2v:
-        tmp = tmp.replace(p, p2v[p])
+    def startup(self):
+        if not (item := input(GUIDER_PANEL).strip()):
+            return
 
-    print(" V2RayN ".center(50, "="), tmp)
-    path_output = os.path.join(config.dir_conf, "v2rayn_naive.json")
-    with open(path_output, "w", encoding="utf8") as file:
-        file.write(tmp)
-
-
-def dropout_client_config_nekoray():
-    tmp = NEKORAY_TEMPLATE
-    p2v = {
-        "[domain]": config.domain,
-        "[username]": config.username,
-        "[password]": config.password,
-        "[port]": config.port,
-    }
-    for p in p2v:
-        tmp = tmp.replace(p, p2v[p])
-
-    share_link = (
-        f"naive+{config.scheme}://{config.username}:{config.password}@{config.domain}:{config.port}#NaiveNode"
-    )
-    print(" NekoRay/Matsuri ".center(50, "="), tmp)
-    print(f"NekoRay/Matsuri 分享链接 {share_link}\n")
-    path_output = os.path.join(config.dir_conf, "nekoray_naive.txt")
-    with open(path_output, "w", encoding="utf8") as file:
-        file.write(tmp)
-
-
-def autorun():
-    logging.info("按任意键部署 Naiveproxy 后台任务")
-    input("")
-    os.system(f"cd {config.dir_workspace} && ./caddy start")
+        if item == "1":
+            self.deploy()
+        elif item == "2":
+            self.delete()
+        elif item == "3":
+            self.utils.caddy_start()
+        elif item == "4":
+            self.utils.caddy_stop()
+        elif item == "5":
+            self.utils.caddy_reload()
+        elif item == "6":
+            self.utils.caddy_status()
+        elif item == "7":
+            self.checkout()
+        elif item == "8":
+            self.reset()
+        elif item == "9":
+            self.upgrade()
+        elif item == " 10":  # sync upstream
+            logging.info("NotImplement")
 
 
 if __name__ == "__main__":
-    preprocess()
-    handle_server()
-    if os.path.isfile(config.path_caddy):
-        guider_input()
-        autorun()
-        dropout_client_config_v2rayn()
-        dropout_client_config_nekoray()
-    else:
-        logging.error("编译失败")
+    try:
+        CMDPanel().startup()
+    except KeyboardInterrupt:
+        print("\n")
+
+# wget -qO /home/npstart.py https://raw.githubusercontent.com/QIN2DIM/np-start/dev/main.py && python3 /home/npstart.py
